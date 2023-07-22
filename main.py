f113df610a0e424c6dace47ed488bcdfc41061b3
Laszer271 <Laszer2710@gmail.com>
2023-07-22 12:25:44 +0200

    updated main for streamlit deployment

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ from selenium.webdriver import FirefoxOptions
 
 import os
 
-@st.experimental_singleton
+
+@st.cache_resource
 def installff():
+  print('installing firefox')
   os.system('sbase install geckodriver')
   os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
